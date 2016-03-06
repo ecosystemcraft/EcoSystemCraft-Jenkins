00ce3963c0b92fa5980623ed39c05180c468856f
ecosystemcraft <ecosystemcraft@gmail.com>
2016-03-06 10:29:48 +0900

    feature(init): create docker-machine on azure

---
+++ config/envioronment.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+export AZURE_SUBSCRIPTION_ID='12345678-abcd-abcd-abcd-123456789012'
+export AZURE_SUBSCRIPTION_CERT='/path/to/cert.pem'
+export AZURE_LOCATION='Japan East'
---
+++ docker-create/azure.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+export FILE_PATH=./config/environment.sh
+. ${FILE_PATH}
+
+export AZURE_RESOURCE_NAME=`date +%Y%m%d%H%M`
+
+docker-machine create -d azure \
+ --azure-subscription-id="${AZURE_SUBSCRIPTION_ID}" \
+ --azure-subscription-cert="${AZURE_SUBSCRIPTION_CERT}" \
+ --azure-location "${AZURE_LOCATION}" \
+ ${AZURE_RESOURCE_NAME}
